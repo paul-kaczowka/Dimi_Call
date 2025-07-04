814ee21fe838a98a3fd8b6ab4f27e36b0a018d0f
paul-kaczowka <60779120+paul-kaczowka@users.noreply.github.com>
2025-07-04 23:35:53 +0200

    fix(build): Injecter les variables d'environnement Supabase

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -98,6 +98,10 @@ export default defineConfig({
           unicodeEscapeSequence: false
         }
       })
-    ]
+    ],
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
+    }
   }
 }) 
